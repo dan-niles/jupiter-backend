88edcc593a6c47835ad732fce75a7ab9fc007c30
Mohamed Mushraf <mushrafmim@gmail.com>
2022-12-17 00:04:47 +0530

    code refactored and semicolons removed

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -1,5 +1,5 @@
-import User from "../models/user.model.js";
-import bcrypt from "bcrypt";
+import User from "../models/user.model.js"
+import bcrypt from "bcrypt"
 
 // Insert new user into the database
 export const create = (req, res) => {
@@ -7,29 +7,24 @@ export const create = (req, res) => {
 	if (!req.body) {
 		res.status(400).send({
 			message: "Content can not be empty!",
-		});
+		})
 	}
 
 	// Encrypt password before storing in database
-	const password_hash = bcrypt.hashSync(req.body.password, 10);
+	const password_hash = bcrypt.hashSync(req.body.password, 10)
 
 	// Create a user object
-	const user = new User({
-		emp_id: req.body.emp_id,
-		role: req.body.role,
-		username: req.body.username,
-		password: password_hash,
-	});
+	const user = new User({ ...req.body, password: password_hash })
 
 	// Save user in the database
-	User.create(user, (err, data) => {
+	user.create((err, data) => {
 		if (err)
 			res.status(500).send({
 				message: err.message || "Some error occurred while creating the user",
-			});
-		else res.send(data);
-	});
-};
+			})
+		else res.send(data)
+	})
+}
 
 // Find a single user with a user_id
 export const findOne = (req, res) => {
@@ -38,15 +33,15 @@ export const findOne = (req, res) => {
 			if (err.kind === "not_found") {
 				res.status(404).send({
 					message: `No user found with user_id ${req.params.user_id}`,
-				});
+				})
 			} else {
 				res.status(500).send({
 					message: "Error retrieving user with user_id " + req.params.user_id,
-				});
+				})
 			}
-		} else res.send(data);
-	});
-};
+		} else res.send(data)
+	})
+}
 
 // Retrieve all users from the database
 export const findAll = (req, res) => {
@@ -54,10 +49,10 @@ export const findAll = (req, res) => {
 		if (err)
 			res.status(500).send({
 				message: err.message || "Some error occurred while retrieving users",
-			});
-		else res.send(data);
-	});
-};
+			})
+		else res.send(data)
+	})
+}
 
 // Update a user identified by the user_id in the request
 export const update = (req, res) => {
@@ -65,37 +60,41 @@ export const update = (req, res) => {
 	if (!req.body) {
 		res.status(400).send({
 			message: "Content can not be empty!",
-		});
+		})
 	}
 
-	User.updateById(req.params.user_id, new User(req.body), (err, data) => {
+	const user = new User(req.body)
+
+	user.user_id = req.params.user_id
+
+	user.updateById((err, data) => {
 		if (err) {
 			if (err.kind === "not_found") {
 				res.status(404).send({
 					message: `No user found with user_id ${req.params.user_id}`,
-				});
+				})
 			} else {
 				res.status(500).send({
 					message: "Error updating user with user_id " + req.params.user_id,
-				});
+				})
 			}
-		} else res.send(data);
-	});
-};
+		} else res.send(data)
+	})
+}
 
 // Delete a user with the specified user_id in the request
-export const deleteRecord = (req, res) => {
+export const remove = (req, res) => {
 	User.remove(req.params.user_id, (err, data) => {
 		if (err) {
 			if (err.kind === "not_found") {
 				res.status(404).send({
 					message: `No user found with user_id ${req.params.user_id}`,
-				});
+				})
 			} else {
 				res.status(500).send({
 					message: "Could not delete user with user_id " + req.params.user_id,
-				});
+				})
 			}
-		} else res.send({ message: `User was deleted successfully!` });
-	});
-};
+		} else res.send({ message: `User was deleted successfully!` })
+	})
+}
