eee0cd65880199d9e9f4c22bb9c520ac41e3f29c
Mohamed Mushraf <mushrafmim@gmail.com>
2022-12-17 00:22:52 +0530

    new role based authorization implemented.

--- app/middleware/auth.middleware.js
+++ app/middleware/auth.middleware.js
@@ -1,22 +1,47 @@
-import jwt from "jsonwebtoken";
-import dotenv from "dotenv";
+import jwt from "jsonwebtoken"
+import dotenv from "dotenv"
 
-dotenv.config();
+const authorization = {
+	"admin": [
+		"/user"
+	],
+	"hr-user": [
+		"/user"
+	],
+	"user": [
+	]
+}
+
+dotenv.config()
 
 const validateToken = (req, res, next) => {
-	const token =
-		req.headers["access-token"] || req.body.token || req.query.token;
+
+	const token = req.get("authorization")?.slice("Bearer ".length)
 
 	if (!token) {
-		return res.status(403).send("A token is required for authentication");
+		return res.status(403).send("A token is required for authentication")
 	}
-	try {
-		const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
-		req.user = decoded;
-	} catch (err) {
-		return res.status(401).send("Invalid Token");
-	}
-	return next();
-};
 
-export default validateToken;
+	try {
+
+		const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY)
+		req.user = decoded
+
+		const access_routes = authorization[decoded.role]
+
+
+		for (const url of access_routes) {
+			if (req.url.startsWith(url)) {
+				return next()
+			}
+		}
+
+		return res.status(403).send("Access denied.!")
+	} catch (err) {
+
+		console.log(err);
+		return res.status(401).send("Invalid Token")
+	}
+}
+
+export default validateToken
