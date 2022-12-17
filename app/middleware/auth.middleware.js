d94f5fc5229f3fc03c9e22626fa999603dacf8b2
Dan Niles <56271899+dan-niles@users.noreply.github.com>
2022-12-17 19:45:30 +0530

    Fix login authentication bug

--- app/middleware/auth.middleware.js
+++ app/middleware/auth.middleware.js
@@ -10,6 +10,7 @@ const validateToken = (req, res, next) => {
 	if (!token) {
 		return res.status(403).send("A token is required for authentication");
 	}
+
 	try {
 		const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
 		req.user = decoded;
--- server.js
+++ server.js
@@ -1,33 +1,33 @@
-import express from "express"
-import cors from "cors"
+import express from "express";
+import cors from "cors";
 
 // Routes
-import userRoutes from "./app/routes/user.routes.js"
-import loginRoutes from "./app/routes/login.routes.js"
-import validateToken from "./app/middleware/auth.middleware.js"
+import userRoutes from "./app/routes/user.routes.js";
+import loginRoutes from "./app/routes/login.routes.js";
+import validateToken from "./app/middleware/auth.middleware.js";
 
-const app = express()
+const app = express();
 
-app.use(cors())
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-const port = process.env.PORT || 8080
+const port = process.env.PORT || 8080;
 
 // App entrypoint
 app.get("/", (req, res) => {
-	res.json({ message: "Welcome to Jupiter HRM Backend" })
-})
+	res.json({ message: "Welcome to Jupiter HRM Backend" });
+});
 
-app.use("/login", loginRoutes)
+// Initializaing login routes
+app.use("/api/login", loginRoutes);
 
-// Using authentication for the below 
-app.use(validateToken)
-
+// Using authentication for the below
+app.use(validateToken);
 
 // Initializaing routes
-app.use("/user", userRoutes)
+app.use("/api/user", userRoutes);
 
 app.listen(port, () => {
-	console.log(`Server is running on port ${port}.`)
-})
+	console.log(`Server is running on port ${port}.`);
+});
