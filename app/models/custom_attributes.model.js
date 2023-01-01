c4c9fda5be77380da29c3be3890a7755f949ea6f
Dan Niles <56271899+dan-niles@users.noreply.github.com>
2023-01-01 11:52:00 +0530

    Create custom attributes controller

--- app/models/custom_attributes.model.js
+++ app/models/custom_attributes.model.js
@@ -1,44 +1,36 @@
-import connection from "../config/db";
+import connection from "../config/db.js";
 
 class CustomAttributes {
-    constructor({
-        atttr_id,
-        attr_name,
-        alias,
-        data_type
+	constructor({ atttr_id, attr_name, alias, data_type }) {
+		this.atttr_id = atttr_id;
+		this.attr_name = attr_name;
+		this.alias = alias;
+		this.data_type = data_type;
+	}
 
-    })
+	static getAll(result) {
+		connection.query(`SELECT * FROM custom_attribute`, result);
+	}
 
-    {
-        this.atttr_id = atttr_id;
-        this.attr_name = attr_name;
-        this.alias = alias;
-        this.data_type = data_type;
-    }
+	static getById(atttr_id, result) {
+		connection.query(
+			`SELECT * FROM custom_attribute WHERE atttr_id = ?`,
+			[atttr_id],
+			result
+		);
+	}
 
-    static getAll(result) {
-        connection.query(`SELECT * FROM custom_attributes`, result);
-    }
+	static getByField(attr_name, result) {
+		connection.query(
+			`SELECT * FROM custom_attribute WHERE attr_name = ?`,
+			[attr_name],
+			result
+		);
+	}
 
-    static getById(atttr_id, result) {
-        connection.query(
-            `SELECT * FROM custom_attributes WHERE atttr_id = ?`,
-            [atttr_id],
-            result
-        )
-    }
-
-    static getByField(attr_name, result) {
-        connection.query(
-            `SELECT * FROM custom_attributes WHERE attr_name = ?`,
-            [attr_name],
-            result
-        )
-    }
-
-    create(result) {
-        connection.query(
-            `INSERT INTO custom_attributes
+	create(result) {
+		connection.query(
+			`INSERT INTO custom_attribute
             (
                 atttr_id,
                 attr_name,
@@ -47,41 +39,30 @@ class CustomAttributes {
             )
             VALUES
             (?,?,?,? )`,
-            [
-                this.atttr_id,
-                this.attr_name,
-                this.alias,
-                this.data_type
-            ],
-            result
-        )
-    }
+			[this.atttr_id, this.attr_name, this.alias, this.data_type],
+			result
+		);
+	}
 
-    update(result) {
-        connection.query(
-            `UPDATE custom_attributes SET
+	update(result) {
+		connection.query(
+			`UPDATE custom_attribute SET
             attr_name = ?,
             alias = ?,
             data_type = ?
             WHERE atttr_id = ?`,
-            [
-                this.attr_name,
-                this.alias,
-                this.data_type,
-                this.atttr_id
-            ],
-            result
-        )
-    }
+			[this.attr_name, this.alias, this.data_type, this.atttr_id],
+			result
+		);
+	}
 
-    delete(result) {
-        connection.query(
-            `DELETE FROM custom_attributes WHERE atttr_id = ?`,
-            [this.atttr_id],
-            result
-        )
-    }
-
+	delete(result) {
+		connection.query(
+			`DELETE FROM custom_attribute WHERE atttr_id = ?`,
+			[this.atttr_id],
+			result
+		);
+	}
 }
 
 export default CustomAttributes;
--- server.js
+++ server.js
@@ -8,6 +8,7 @@ import userRoutes from "./app/routes/user.routes.js";
 import loginRoutes from "./app/routes/login.routes.js";
 import employeeRoutes from "./app/routes/employee.routes.js";
 import orgInfoRoutes from "./app/routes/org_info.routes.js";
+import customAttrRoutes from "./app/routes/custom_attributes.routes.js";
 
 const app = express();
 
@@ -32,6 +33,7 @@ app.use("/api/login", loginRoutes);
 app.use("/api/user", userRoutes);
 app.use("/api/employee", employeeRoutes);
 app.use("/api/org_info", orgInfoRoutes);
+app.use("/api/custom_attributes", customAttrRoutes);
 
 app.listen(port, () => {
 	console.log(`Server is running on port ${port}.`);
